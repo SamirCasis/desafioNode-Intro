fdd3b69f9b8a124f9bb01400f5a20d7014421416
samir <samir.casis@gmail.com>
2024-05-18 12:25:49 -0400

    corrección en slice [] por ()  y cambios en  operaciones.js, ok 100%

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { registrar, leer } = require('./operaciones.js');
 
-const [operacion, id, nombre, edad, mascota, color, enfermedad] = process.argv.slice[2];
+const [operacion, id, nombre, edad, mascota, color, enfermedad] = process.argv.slice(2);
 
 if (operacion === 'registrar') {
     registrar(id, nombre, edad, mascota, color, enfermedad)
--- operaciones.js
+++ operaciones.js
@@ -2,12 +2,12 @@ const fs = require('fs');
 
 const registrar = (nombre, edad, mascota, color, enfermedad) => {
     const citas = JSON.parse(fs.readFileSync('citas.json', 'utf-8'))
-    const nuevoId = Math.floor(Math.random() * 50).toString();
+    const id = Math.floor(Math.random() * 50).toString();
     if (!nombre || !edad || !mascota || !color || !enfermedad) {  // verifica que se ingresen todos los datos
         console.error('Debes ingresar la totalidad de los datos')
         return; // retorna el mensaje de error
     }
-    citas.push({ nuevoId, nombre, edad, mascota, color, enfermedad })
+    citas.push({ id, nombre, edad, mascota, color, enfermedad })
     fs.writeFileSync('citas.json', JSON.stringify(citas))
 };
 
